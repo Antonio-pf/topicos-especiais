f67cca7507f25bbb42dc57bb5a36b78bdaa47d97
Antonio-pf <antoniopfelipe03@gmail.com>
2024-08-16 21:23:24 -0300

    fix: subtraindo valor produto

--- aula02/exer01.py
+++ aula02/exer01.py
@@ -17,13 +17,13 @@ def setInfoProduto():
 def setDesconto(valorProduto):
 
     if valorProduto <= 50 and valorProduto < 200:
-        infoProduto['desconto'] = 0.05 * infoProduto['valor'] + infoProduto['valor']
+        infoProduto['desconto'] = infoProduto['valor'] - 0.05 * infoProduto['valor']  
     elif valorProduto >= 200 and valorProduto < 500:
-        infoProduto['desconto'] = 0.06 * infoProduto['valor'] + infoProduto['valor']
+        infoProduto['desconto'] = infoProduto['valor'] - 0.06 * infoProduto['valor'] 
     elif valorProduto >= 500 and valorProduto < 1000:
-        infoProduto['desconto'] = 0.07 * infoProduto['valor'] + infoProduto['valor']
+        infoProduto['desconto'] = infoProduto['valor'] - 0.07 * infoProduto['valor']  
     elif valorProduto >= 1000:
-        infoProduto['desconto'] = 0.08 * infoProduto['valor'] + infoProduto['valor']
+        infoProduto['desconto'] = infoProduto['valor'] - 0.08 * infoProduto['valor'] 
 
 
 setInfoProduto()
